13a9154d4776a8cc4ab33b0eb6266fce4b6465f8
nvt1504 <159101297+nvt1504@users.noreply.github.com>
2024-11-23 09:47:48 +0700

    Update and rename Cart.java to ShoppingCart.java

---
+++ Lab02/AimsProject/ShoppingCart.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class ShoppingCart {
+    private ArrayList<DigitalVideoDisc> dvdList;
+    private final int capacity;
+
+    public ShoppingCart() {
+        this.dvdList = new ArrayList<>();
+        this.capacity = 20; // Giới hạn tối đa cho giỏ hàng
+    }
+
+    public ArrayList<DigitalVideoDisc> getDvdList() {
+        return dvdList;
+    }
+
+    public void addDisc(DigitalVideoDisc disc) {
+        if (dvdList.size() < capacity) {
+            dvdList.add(disc);
+            System.out.println("DVD \"" + disc.getTitle() + "\" đã được thêm vào giỏ hàng.");
+        } else {
+            System.out.println("Không thể thêm DVD. Giỏ hàng đã đầy.");
+        }
+    }
+
+    public boolean removeDisc(DigitalVideoDisc disc) {
+        boolean isRemoved = dvdList.remove(disc);
+        if (isRemoved) {
+            System.out.println("DVD \"" + disc.getTitle() + "\" đã được xóa khỏi giỏ hàng.");
+        } else {
+            System.out.println("DVD \"" + disc.getTitle() + "\" không tìm thấy trong giỏ hàng.");
+        }
+        return isRemoved;
+    }
+
+    public float calculateTotalPrice() {
+        float totalPrice = 0;
+        for (DigitalVideoDisc disc : dvdList) {
+            totalPrice += disc.getCost();
+        }
+        return totalPrice;
+    }
+
+    public void showItems() {
+        System.out.println("Giỏ hàng có " + dvdList.size() + " DVD:");
+        for (DigitalVideoDisc disc : dvdList) {
+            disc.displayInfo();
+            System.out.println();
+        }
+        System.out.println("Tổng giá: $" + calculateTotalPrice());
+    }
+}
