94e49fe0e7dbdbdfb03fdb734babd0492486ce3c
Nguyen Viet Thanh <kiaisoft.thanh.nguyenviet@gmail.com>
2024-11-24 00:56:34 +0700

    bai9

---
+++ Lab02/OtherProjects/hust.soict.dsai.garbage/NoGarbage.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class NoGarbage {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        try {
+            // Đọc tệp "largefile.txt" sử dụng StringBuffer để nối chuỗi
+            FileInputStream fileInputStream = new FileInputStream("largefile.txt");
+            int content;
+            while ((content = fileInputStream.read()) != -1) {
+                sb.append((char) content); // Dùng StringBuffer để nối chuỗi
+            }
+            fileInputStream.close();
+            // Chuyển StringBuffer thành String cuối cùng
+            String result = sb.toString();
+        } catch (IOException e) {
+            System.out.println("Lỗi khi đọc tệp: " + e.getMessage());
+        }
+    }
+}
